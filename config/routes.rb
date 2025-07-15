41b523cb2b0e01fb28717735af6b299625597d09
EC2 Default User <ec2-user@ip-172-31-6-125.ap-northeast-1.compute.internal>
2025-07-15 07:30:40 +0000

    commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  
+ devise_for :users
+ root 'homes#top'
+ resources :post_images, only: [:new, :create, :index, :show, :destroy]
+ get 'homes/about', to: 'homes#about', as: 'about'
+end
